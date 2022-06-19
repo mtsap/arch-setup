daa66c06e20cfeab04dd9c5abf8d419dbff4bddb
Michael Tsapanos <michael.tsapanos@ato-gear.com>
2022-06-19 16:11:38 +0200

    fix zsh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -4,13 +4,17 @@ sudo pacman -S --noconfirm --needed zsh zsh-completions
 if [ $SHELL == "/bin/bash" ]
 then 
      echo "Changling shell to zsh"
-     chsh -s (which zsh)
-if
+     chsh -s $( which zsh )
+fi
 
   # TODO:
   # check if oh-my-zsh exist and install it
 if [ -d "$HOME/.oh-my-zsh" ]
 then
+  echo "oh my zsh already installed"
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else 
+  echo "installing oh my zsh"
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
   # check if p10k exist and install it
