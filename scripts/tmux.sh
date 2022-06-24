35c114b05c1006b73e9a1937597a9ae4d6871d54
mtsap <mich.tsap@gmail.com>
2022-06-24 12:18:29 +0200

    Fix tmux

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S --noconfirm --needed tmux 
+sudo pacman -S --noconfirm --needed tmux python3 python-pip python-libtmux 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux source ~/.tmux.conf
 ~/.tmux/plugins/tpm/bin/install_plugins
