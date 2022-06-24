82132770805aad8c7b367b4f1feed794bfee8df7
Michael Tsapanos <michael.tsapanos@ato-gear.com>
2022-06-24 13:18:27 +0200

    Fix tmux

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -3,5 +3,6 @@
 sudo pacman -S --noconfirm --needed tmux python3 python-pip python-libtmux 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux source ~/.tmux.conf
+tmux new-session -d
 ~/.tmux/plugins/tpm/bin/install_plugins
 
