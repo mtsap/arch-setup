8fb9f5734d153e0b0b23f7e1e5995ba246ccd08f
Michael Tsapanos <michael.tsapanos@ato-gear.com>
2022-08-18 21:01:47 +0200

    More programs

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -15,7 +15,8 @@ fzf \
 sudo pacman -S --noconfirm atool p7zip unrar unzip xz zip bzip2 gzip tar
 
 #download utils
-sudo pacman -S --noconfirm --needed aria2 curl wget
+sudo pacman -S --noconfirm --needed aria2 curl wget dunst dmenu xmonad \
+xmonad-contrib \
 
 #instaa programs
 sudo pacman -S --noconfirm --needed chromium gimp vlc
