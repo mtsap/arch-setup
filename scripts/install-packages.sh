65c049934030ed8b3d6290698addea9728f28b5c
Michael Tsapanos <mich.tsap@gmail.com>
2025-05-24 17:56:43 +0300

    more programs

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -4,15 +4,15 @@ sudo pacman -S --noconfirm --needed git lsd tree base-devel source-highlight \
 	fzf kitty rofi-wayland thunar thunar-archive-plugin linux-headers
 
 # unzip tools
-sudo pacman -S --noconfirm atool p7zip unrar unzip xz zip bzip2 gzip tar
+sudo pacman -S --noconfirm atool p7zip unrar unzip xz zip bzip2 gzip tar bat
 
 #download utils
 sudo pacman -S --noconfirm --needed aria2 curl wget \
-	 eza light ponymix bluez bluez-utils python-gobject
+	 eza light ponymix bluez bluez-utils python-gobject git-delta
 
 #instaa programs
 sudo pacman -S --noconfirm --needed chromium gimp vlc zsh-theme-powerlevel10k \
-	  bitwarden-cli  gvfs file-roller noto-fonts-emoji exa
+	  bitwarden-cli  gvfs file-roller noto-fonts-emoji eza
 
 #instaa programs
 sudo pacman -S --noconfirm --needed chromium gimp vlc \
